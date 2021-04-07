9c2af172f171b6a0ba508c0351bf40ef567a63a9
Tatum Bray <tatumrbray@gmail.com>
2021-04-06 18:29:47 -0600

    Main, CampsiteInfo, and Directory Component, week1

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import Main from "./components/Main"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style='auto' />
-    </View>
-  )
+  return <Main />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0ff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
