db2f7012b621f31fdd6d4d2904ff76702ad945a7
Tatum Bray <tatumrbray@gmail.com>
2021-04-16 10:42:50 -0600

    Activity Indicator

--- components/About.js
+++ components/About.js
@@ -3,6 +3,7 @@ import { Card, ListItem } from "react-native-elements"
 import { FlatList, ScrollView, Text } from "react-native"
 import { connect } from "react-redux"
 import { baseUrl } from "../shared/baseUrl"
+import Loading from "./Loading"
 
 const mapStateToProps = (state) => {
   return {
@@ -42,6 +43,26 @@ class About extends Component {
       )
     }
 
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title='Community Partners'>
+            <Loading />
+          </Card>
+        </ScrollView>
+      )
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title='Community Partners'>
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      )
+    }
     return (
       <ScrollView>
         <Mission />
